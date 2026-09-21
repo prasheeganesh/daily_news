59ad1450c16b33dca68dc937be5683d8fbb819c4
prasheeganesh <prasheeganesh@gmail.com>
2026-09-21 21:16:14 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -52,15 +52,16 @@ export default async function handler(req, res) {
         news: news
       };
 
-      await put(
-        "latest-news.json",
-        JSON.stringify(data),
-        {
-          access: "private",
-          addRandomSuffix: false,
-          contentType: "application/json"
-        }
-      );
+     await put(
+  "latest-news.json",
+  JSON.stringify(data),
+  {
+    access: "private",
+    addRandomSuffix: false,
+    allowOverwrite: true,
+    contentType: "application/json"
+  }
+);
 
       return res.status(200).json({
         success: true,
